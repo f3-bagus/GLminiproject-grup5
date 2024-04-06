e054f808e53ef51aeaa3d03d2eb5454f230a5929
fauqaji <133103837+fauqaji@users.noreply.github.com>
2024-04-06 10:13:16 +0700

    commit

--- script.js
+++ script.js
@@ -1,21 +1,85 @@
 $(document).ready(function() {
-    // Function to check screen width and move elements accordingly
     function moveElements() {
         if ($(window).width() <= 768) {
-            // Move the image element to the top of its parent container
             $('.col-md-6.d-flex.justify-content-end.align-items-center').insertBefore('.col-md-6.mb-4');
         } else {
-            // Move the image element back to its original position
             $('.col-md-6.d-flex.justify-content-end.align-items-center').insertAfter('.col-md-6.mb-4');
         }
     }
 
-    // Initial check on document ready
-    moveElements();
+    function checkPseudoElement() {
+        if ($(window).width() <= 768) {
+            $('.p1-beranda').removeClass('before').addClass('after');
+        } else {
+            $('.p1-beranda').removeClass('after').addClass('before');
+        }
+    }
 
-    // Check on window resize
-    $(window).resize(function() {
+    function checkPadding() {
+        if ($(window).width() <= 768) {
+            $('.p1-beranda').css('padding-left', '0');
+        } else {
+            $('.p1-beranda').css('padding-left', '55px');
+        }
+    }
+
+    function adjustpkata() {
+        if ($(window).width() < 768) {
+            $(".p1-katakata").css("font-size", "30px");
+        } else {
+            $(".p1-katakata").css("font-size", "72px");
+        }
+    }
+
+    function adjustimgtent() {
+        if ($(window).width() < 768) {
+            $(".img-tentang").css({
+                "margin-left": "auto",
+                "width": "230px",
+                "height": "290px"
+            });
+        } else {
+            $(".img-tentang").css({
+                "margin-left": "70px",
+                "width": "320px",
+                "height": "380px"
+            });
+        }
+    }
+
+    function adjustdeskrispiT() {
+        if ($(window).width() < 880) {
+            $(".deskripsiT").css("margin-right", "0");
+        } else {
+            $(".deskripsiT").css("margin-right", "190px");
+        }
+    }
+
+    function adjustPadding() {
+    if ($(window).width() < 768) {
+        $('.container-form').css('padding', '15px');
+    } else if ($(window).width() >= 768 && $(window).width() < 1024) {
+        $('.container-form').css('padding', '2vh 6vh');
+    } else {
+        $('.container-form').css('padding', '10vh 30vh');
+    }
+}
+
+
+    function adjustElements() {
         moveElements();
+        checkPseudoElement();
+        checkPadding();
+        adjustpkata();
+        adjustimgtent();
+        adjustdeskrispiT();
+        adjustPadding();
+    }
+
+    adjustElements();
+
+    $(window).resize(function() {
+        adjustElements();
     });
 
     $('.btnB').mouseenter(function() {
@@ -29,94 +93,54 @@ $(document).ready(function() {
             'color': '#ffffff'
         });
     });
-    
 
-    function checkPseudoElement() {
-    if ($(window).width() <= 768) {
-        $('.p1-beranda').removeClass('before').addClass('after');
-    } else {
-        $('.p1-beranda').removeClass('after').addClass('before');
-    }
-    }
-
-    function checkPadding() {
-        if ($(window).width() <= 768) {
-            $('.p1-beranda').css('padding-left', '0');
-        } else {
-            $('.p1-beranda').css('padding-left', '55px');
-        }
-    }
-
-    // function adjustImgT() {
-    //     if ($(window).width() < 768) { // Jika tampilan mobile
-    //         $(".imgT").css({
-    //             "margin-left": "0",
-    //             "max-width": "300px"
-    //         });
-    //     } else { // Jika tampilan tidak mobile
-    //         $(".imgT").css({
-    //             "margin-left": "300px",
-    //             "max-width": "450px"
-    //         });
-    //     }
-    // }
-    
-    function adjustpkata() {
-        if ($(window).width() < 768) { // Jika tampilan mobile
-            $(".p1-katakata").css({
-                "font-size": "30px"
-            });
-        } else { // Jika tampilan tidak mobile
-            $(".p1-katakata").css({
-                "font-size": "72px"
-            });
-        }
-    }
-
-    function adjustimgtent() {
-        if ($(window).width() < 768) { // Jika tampilan mobile
-            $(".img-tentang").css({
-                "margin-left": "auto",
-                "width" : "230px",
-                "height" : "290px"
-            });
-        } else { // Jika tampilan tidak mobile
-            $(".img-tentang").css({
-                "margin-left": "70px",
-                "width" : "320px",
-                "height" : "380px"
-            });
-        }
-    }
-
-    function adjustdeskrispiT() {
-        if ($(window).width() < 880) { // Jika tampilan mobile
-            $(".deskripsiT").css({
-                "margin-right": "0"
-            });
-        } else { // Jika tampilan tidak mobile
-            $(".deskripsiT").css({
-                "margin-right": "190px"
-            });
-        }
-    }
-
-
-
-    // Initial check on page load
-    checkPseudoElement();
-    checkPadding();
-    adjustpkata();
-    adjustimgtent();
-    adjustdeskrispiT()
-
-    // Check on window resize
-    $(window).resize(function() {
-        checkPseudoElement();
-        checkPadding();
-        adjustpkata();
-        adjustimgtent();
-        adjustdeskrispiT()
+    $('.card').mouseenter(function() {
+        $(this).css({
+            'filter': 'brightness(90%)',
+        });
+    }).mouseleave(function() {
+        $(this).css({
+            'filter': 'brightness(100%)',
+        });
     });
 
+    $('.paket').mouseenter(function() {
+        $(this).css({
+            'filter': 'brightness(80%)',
+        });
+    }).mouseleave(function() {
+        $(this).css({
+            'filter': 'brightness(100%)',
+        });
+    });
+
+    $('.card-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            },
+        }
+    });
+
+    $('#gambar').change(function() {
+        var fileName = $(this).val().split('\\').pop();
+        if (fileName) {
+            $('.upload-btn-img').addClass('file-uploaded').text('Diunggah');
+        } else {
+            $('.upload-btn-img').removeClass('file-uploaded').text('Unggah');
+        }
+    });
+
+    $(window).resize(function() {
+        moveElements();
+    });
 });
